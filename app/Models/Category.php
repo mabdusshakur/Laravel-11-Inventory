af46afaef623083b5a7c88b9ecd54fd5b4aecf73
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-08-21 22:44:54 +0600

    user relation added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,9 @@ class Category extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
