f1ba21f0ec3c9c38710c78f19ac7d2d22810bf6e
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-08-21 15:13:33 +0600

    sendError, sendSuccess func added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,40 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+
+    /**
+     * Summary of sendSuccess
+     * @param mixed $message - success message
+     * @param mixed $result - data to be returned in the response
+     * @param mixed $statusCode - status code, default is 200
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function sendSuccess($message, $result, $statusCode = 200)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+        return response()->json($response, $statusCode);
+    }
+
+
+    /**
+     * Summary of sendError
+     * @param mixed $message - error message
+     * @param mixed $statusCode - error status code, default is 400
+     * @param mixed $errors - error data array
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function sendError($message, $statusCode = 400, $errors = [])
+    {
+        $response = [
+            'success' => false,
+            'message' => $message,
+            'errors' => $errors,
+        ];
+
+        return response()->json($response, $statusCode);
+    }
 }
