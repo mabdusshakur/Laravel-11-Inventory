1c909484227f58ebeb97d016a8fd5017d7e2c36a
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-08-21 12:44:37 +0600

    product table, model, controller, requests added

---
+++ app/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id', 'category_id', 'name', 'price', 'unit', 'img_url'];
+}
