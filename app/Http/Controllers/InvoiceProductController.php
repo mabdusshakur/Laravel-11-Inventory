2fe8864d74f4cad658abcb8602ff0206d04f8533
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-08-21 12:49:51 +0600

    invoice product added

---
+++ app/Http/Controllers/InvoiceProductController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InvoiceProduct;
+use App\Http\Requests\StoreInvoiceProductRequest;
+use App\Http\Requests\UpdateInvoiceProductRequest;
+
+class InvoiceProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvoiceProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InvoiceProduct $invoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InvoiceProduct $invoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInvoiceProductRequest $request, InvoiceProduct $invoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InvoiceProduct $invoiceProduct)
+    {
+        //
+    }
+}
---
+++ app/Models/InvoiceProduct.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceProduct extends Model
+{
+    use HasFactory;
+    protected $fillable = ['quantity', 'sale_price', 'product_id', 'invoice_id', 'user_id'];
+}
