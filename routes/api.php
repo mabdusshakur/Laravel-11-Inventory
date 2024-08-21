e2a3404a481257def015699c0f67aa74f538d89f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-08-21 18:58:11 +0600

    user functionality completed

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Middleware\TokenVerificationMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/user-registration', [UserController::class, 'userRegistration']);
+Route::post('/user-login', [UserController::class, 'userLogin']);
+
+Route::post('/send-otp', [UserController::class, 'sendOtp']);
+Route::post('/verify-otp', [UserController::class, 'verifyOtp']);
+
+Route::group(['middleware' => TokenVerificationMiddleware::class], function () {
+    Route::post('/user-logout', [UserController::class, 'userLogout']);
+    Route::get('/user', [UserController::class, 'getUser']);
+    Route::patch('/reset-password', [UserController::class, 'resetPassword']);
+});
