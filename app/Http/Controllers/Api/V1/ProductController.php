1d8e17c774a3bba5a340a4edab32eca95198d8d8
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-08-22 13:04:09 +0600

    product controller finished

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProductRequest;
@@ -14,7 +15,12 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $products = Product::where('user_id', $this->getUserId(request()))->get();
+            return $this->sendSuccess("Product list", ProductResource::collection($products));
+        } catch (\Throwable $th) {
+            return $this->sendError("Failed to get Product list", 500, $th->getMessage());
+        }
     }
 
     /**
@@ -30,15 +36,46 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request)
     {
-        //
+        try {
+            $img_url = '';
+            if ($request->file('image')) {
+                $image = $request->file('image');
+                $image_name = time() . '.' . $image->extension();
+                $image->move(public_path('images'), $image_name);
+                $img_url = "images/" . $image_name;
+            }
+
+            $product = Product::create([
+                'name' => $request->name,
+                'price' => $request->price,
+                'unit' => $request->unit,
+                'img_url' => $img_url,
+                'category_id' => $request->category_id,
+                'user_id' => $request->user_id,
+            ]);
+            if (!$product) {
+                return $this->sendError("Failed to Create Product", 500);
+                if ($img_url) {
+                    unlink($img_url);
+                }
+            }
+            return $this->sendSuccess("Product Created", new ProductResource($product), 201);
+        } catch (\Throwable $th) {
+            return $this->sendError("Failed to Create Product", 500, $th->getMessage());
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show($product)
     {
-        //
+        try {
+            $product = Product::where('user_id', $this->getUserId(request()))->findOrFail($product);
+            return $this->sendSuccess("Product Details", new ProductResource($product));
+        } catch (\Throwable $th) {
+            return $this->sendError("Failed to get Product Details", 500, $th->getMessage());
+        }
     }
 
     /**
@@ -52,16 +89,55 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(UpdateProductRequest $request, $product)
     {
-        //
+        try {
+            $product = Product::where('user_id', $this->getUserId(request()))->findOrFail($product);
+            $product->name = $request->name;
+            $product->price = $request->price;
+            $product->unit = $request->unit;
+            $product->category_id = $request->category_id;
+            $product->user_id = $request->user_id;
+
+
+            // if new image is uploaded, delete the old image and save the new image url on db column
+            $new_img_url = null;
+            if ($request->file('image')) {
+                $image = $request->file('image');
+                $image_name = time() . '.' . $image->extension();
+                $image->move(public_path('images'), $image_name);
+                $new_img_url = "images/" . $image_name;
+                unlink($product->img_url);
+            }
+
+            $product->img_url = $new_img_url ? $new_img_url : $product->img_url;
+            $product->save();
+
+            if (!$product) {
+                return $this->sendError("Failed to Update Product", 500);
+            }
+            return $this->sendSuccess("Product Updated", new ProductResource($product));
+        } catch (\Throwable $th) {
+            return $this->sendError("Failed to Update Product", 500, $th->getMessage());
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy($product)
     {
-        //
+        try {
+            $product = Product::where('user_id', $this->getUserId(request()))->findOrFail($product);
+            if (!$product->delete()) {
+                return $this->sendError("Failed to Delete Product", 500);
+            }
+            if ($product->img_url) {
+                unlink($product->img_url);
+            }
+            return $this->sendSuccess("Product Deleted", []);
+        } catch (\Throwable $th) {
+            return $this->sendError("Failed to Delete Product", 500, $th->getMessage());
+        }
     }
 }
