cb9ff07a0600e2d235a8b3074d6e6f30a2574534
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-08-22 11:02:44 +0600

    category-user relations added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,14 @@ class Product extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id', 'category_id', 'name', 'price', 'unit', 'img_url'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
