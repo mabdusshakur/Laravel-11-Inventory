5652fe3789e35a4673afc5351dd138d79754147b
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-08-22 16:41:59 +0600

    user-auth web routes added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login-page', [AuthController::class, 'loginPage'])->name('web.login');
+Route::get('/register-page', [AuthController::class, 'registerPage'])->name('web.register');
+Route::get('/send-otp-page', [AuthController::class, 'sendOtpPage'])->name('web.send-otp');
+Route::get('/verify-otp-page', [AuthController::class, 'verifyOtpPage'])->name('web.verify-otp');
+Route::get('/reset-password-page', [AuthController::class, 'resetPasswordPage'])->name('web.reset-password');
+
+Route::get('/dashboard', function () {
+    return "Dashboard";
+})->name('web.dashboard');
