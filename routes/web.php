fb7d411e61a81d37b158a356027a6db6447640bb
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-08-23 10:02:10 +0600

    basic page routes added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login-page', [AuthController::class, 'loginPage'])->name('web.login');
@@ -9,6 +10,10 @@ Route::get('/send-otp-page', [AuthController::class, 'sendOtpPage'])->name('web.
 Route::get('/verify-otp-page', [AuthController::class, 'verifyOtpPage'])->name('web.verify-otp');
 Route::get('/reset-password-page', [AuthController::class, 'resetPasswordPage'])->name('web.reset-password');
 
-Route::get('/dashboard', function () {
-    return "Dashboard";
-})->name('web.dashboard');
+Route::get('/dashboard', [DashboardController::class, 'dashboardPage'])->name('web.dashboard');
+Route::get('/customer', [DashboardController::class, 'customerPage'])->name('web.customer');
+Route::get('/category', [DashboardController::class, 'categoryPage'])->name('web.category');
+Route::get('/product', [DashboardController::class, 'productPage'])->name('web.product');
+Route::get('/sale', [DashboardController::class, 'salePage'])->name('web.sale');
+Route::get('/invoice', [DashboardController::class, 'invoicePage'])->name('web.invoice');
+Route::get('/report', [DashboardController::class, 'reportPage'])->name('web.report');
